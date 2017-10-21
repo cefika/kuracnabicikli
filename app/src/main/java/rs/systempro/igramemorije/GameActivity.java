ac296c087af4171666c11fc7af0f16a416b8d025
cefika <cefika99@live.com>
2017-10-21 11:38:17 +0200

    Subota ujutu

--- app/src/main/java/rs/systempro/igramemorije/GameActivity.java
+++ app/src/main/java/rs/systempro/igramemorije/GameActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -12,30 +13,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static rs.systempro.igramemorije.R.id.glMreza;
+import static rs.systempro.igramemorije.R.id.image;
 
 public class GameActivity extends AppCompatActivity {
 
-    int h;
-    int w;
+    int solve;
+    ImageButton firstOpened;
+    ImageButton secondOpened;
+    int numofOpened;
+    int time;
     ArrayList<Drawable> images;
 
+    @RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
         Intent myIntent = getIntent();
-        h= myIntent.getIntExtra("height", 4);
-        w = myIntent.getIntExtra("width",3);
+        final int h= myIntent.getIntExtra("height", 4);
+        final int w = myIntent.getIntExtra("width",3);
+
         images=new ArrayList<>();
+        solve=0;
+        numofOpened=0;
         for(int i=0;i<(h*w)/2;i++)
         {
             int resId= getResources().getIdentifier("slicica"+(i+1),"drawable",getPackageName());
@@ -61,12 +73,23 @@ public class GameActivity extends AppCompatActivity {
                 int rootW=rlRoot.getWidth();
                 Toast.makeText(getApplicationContext(),rootH+" - "+rootW,Toast.LENGTH_SHORT).show();
                 for(int i=0;i<h*w;i++){
-                    ImageButton b = new ImageButton(getApplicationContext());
-
+                   final ImageButton b = new ImageButton(getApplicationContext());
+                    final Drawable image=images.get(i);
                    // b.setText("Dugme "+ i);
                     b.setBackgroundColor(Color.RED);
-                    b.setImageDrawable(images.get(i));
+                    //b.setImageDrawable(images.get(i));
+                    b.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    b.setPadding(0,0,0,0);
+
                     glmreza.addView(b);
+                    time=0;
+                    Timer t=new Timer();
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            time++;
+                        }
+                    },1000,1000);
                     GridLayout.LayoutParams param =new GridLayout.LayoutParams();
                     if(rootH/h<rootW/w) {
                         param.height = rootH / h - 20;
@@ -80,6 +103,38 @@ public class GameActivity extends AppCompatActivity {
                         }
                     param.setMargins(10,10,10,10);
                     b.setLayoutParams(param);
+                    b.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                           // b.setImageDrawable(image);
+                            if(numofOpened==2)
+                            {
+                                numofOpened=1;
+                                firstOpened.setImageDrawable(null);
+                                secondOpened.setImageDrawable(null);
+                                firstOpened=b;
+                                b.setImageDrawable(image);
+                            }
+                            else if(numofOpened==1)
+                                {
+                                    b.setImageDrawable(image);
+                                    numofOpened++;
+                                    secondOpened=b;
+                                    if(firstOpened.getDrawable().getConstantState().equals(secondOpened.getDrawable().getConstantState())) {
+                                        numofOpened=0;
+                                        firstOpened.setClickable(false);
+                                        secondOpened.setClickable(false);
+                                    }
+                                }
+                            else
+                                {
+                                    b.setImageDrawable(image);
+                                    numofOpened=1;
+                                    firstOpened=b;
+                                }
+                            //b.setImageDrawable(image);
+                        }
+                    });
                 }
             }
         });
